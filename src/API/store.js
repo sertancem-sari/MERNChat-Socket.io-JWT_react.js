e152e26e2399acaca0884a90e91d7b2dc844d9ca
sertancem-sari <cem_sertan@hotmail.com>
2023-04-09 23:35:21 +0300

    need to continue on jwt

--- src/API/store.js
+++ src/API/store.js
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./apiSlice";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import authReducer from '../features/auth/authSlice'
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,//dynamically referring apiSLice
+        auth: authReducer
     },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(apiSlice.middleware),//with getDefaultMiddleware you can get default also extra middlewares 
     devTools: true //default true
 })
+
+setupListeners(store.dispatch) //for refetchOnFocus and refetchOnReconnect behaviors.
